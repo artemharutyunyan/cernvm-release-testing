f84be6ea80f8899541631490e12db35b29b6ba9c
gsoc.student@gmail.com <gsoc.student@gmail.com@e1b191a7-760c-b37a-3d11-b84f7cd3b9c7>
2011-06-06 04:36:08 +0000

    ADD: Added initial cernvm-tests file which runs a very simple test on the test machine to verify that the script executes and can send the TAP report to the server
    git-svn-id: http://cernvm-release-testing.googlecode.com/svn@9 e1b191a7-760c-b37a-3d11-b84f7cd3b9c7

---
+++ trunk/tapper/tapper-autoreport/cernvm-tests.sh
@@ -0,0 +1,64 @@
+#! /bin/bash
+
+###################################################################
+#
+# This script contains several sample tests, to test the features
+# and functionality of tapper, as well as to demo some sample
+# tests specific to cernvm-testing
+#
+# The following tests are executed
+#
+#  - Create/configure, start, and control virtual machines
+#  - CernVM TestCase 1: No error messages at boot
+#  - CernVM TestCase 5: Check for correct time / running ntpd
+#  - CernVM TestCase 6: Check login via ssh
+#
+###################################################################
+
+. ./tapper-autoreport --import-utils
+
+TAP[0]="ok - autoreport selftest"
+#TAP[1]="ok - some other description"
+#append_tap "ok - the simplest of all tests"
+
+#HEADERS[0]="# Tapper-KVM-Version: 4.0.1"
+#HEADERS[1]="# Tapper-CernVM-Changeset: 1234"
+#HEADERS[2]="# Tapper-KVM-Base-OS-description: CernVM 2.3.0"
+
+#OUTPUT[0]="I am misc output"
+#OUTPUT[1]="I am put near the end"
+#OUTPUT[2]="Care for me or I disappear"
+
+TAPDATA[0]="timecpb: 12"
+TAPDATA[1]="timenocpb: 15"
+TAPDATA[2]="ratio: 0.8"
+TAPDATA[3]="vendor: $(get_vendor)"
+TAPDATA[4]="cpu_family: $(get_cpu_family)"
+append_tapdata "number_of_tap_reports: $(get_tap_counter)"
+
+main_after_hook ()
+{
+    valuex=17
+    echo "# Be sure to review the attached files, in particular"
+    echo "# the boot_error.log incase there was an error that"
+    echo "# did not get caught by the tests."
+}
+
+SUITENAME="CernVM-Sample-Tests"
+SUITEVERSION="1.00"
+OSNAME="Debian Squeeze"
+VMNAME="cernvm"
+VM_XML_DEFINITION="/home/cernvm/image/cernvm.xml" # Virtual machine XML config file
+CHANGESET="0"
+HOSTNAME="cernvm-host"
+GUESTIP="192.168.122.179"
+TAPPER_REPORT_SERVER="cernvm-server"
+REPORTGROUP=selftest-`date +%Y-%m-%d | md5sum | cut -d" " -f1`
+NOSEND=0
+NOUPLOAD=0
+
+# A very simple test
+uname -a | grep -q Linux  # example of an ok exit code
+ok $? "The system is running Linux"
+
+. ./tapper-autoreport $BOOT_ERRORS
