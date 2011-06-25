be73723c15217458b76bb71e59f75d163893b8dc
gsoc.student@gmail.com <gsoc.student@gmail.com@e1b191a7-760c-b37a-3d11-b84f7cd3b9c7>
2011-06-25 23:41:55 +0000

    ADD: Added support for tests using VirtualBox hypervisor ADD: Added support for web_create_user() to check that creating a new user through the web interface succeeded FIX: Added rudimentary precondition support for boot_check to verify that there is SSH access as root before checking for boot errors as this results in false reports of the test passing as there are no boot errors to find errors in FIX: Had to use literal quotes for regular expression as the \ and spaces for multi-line regular expressions were being interpreted literally when using double quotes (BASH syntactic sauerkraut again)
    git-svn-id: http://cernvm-release-testing.googlecode.com/svn@52 e1b191a7-760c-b37a-3d11-b84f7cd3b9c7

--- trunk/tapper/tapper-autoreport/cernvm-tests.sh
+++ trunk/tapper/tapper-autoreport/cernvm-tests.sh
@@ -23,10 +23,9 @@
 #	Precondition Test 7 - Verify that it is possible to login on web interface
 #						  
 #
-#
-#  	CernVM Test Case 1 - No error messages at boot
-#  	CernVM Test Case 2 - Check for correct time / running ntpd
-#  	CernVM Test Case 3 - Check login via ssh
+#	CernVM Test Case 1 - Check login via ssh as root
+#  	CernVM Test Case 2 - No error messages at boot
+#  	CernVM Test Case 3 - Check for correct time / running ntpd
 #	CernVM Test Case 4 - Create a new user using the CernVM web interface
 #	CernVM Test Case 5 - Verify that the user is created and can be accessed
 #						 from ssh login
@@ -67,20 +66,20 @@ main_after_hook ()
     echo "# did not get caught by the tests."
 }
 
-SUITENAME="CernVM-Sample-Tests"
+SUITENAME="CernVM-VirtualBox-Tests"
 SUITEVERSION="1.00"
-OSNAME="Red Hat 5"
-VMNAME="cernvm"
-VM_XML_DEFINITION="/home/CernVM/IMAGES/kvm/cernvm.xml" # Virtual machine XML config file
+OSNAME="Fedora 13"
+VMNAME="cernvm-vbox"
+VM_XML_DEFINITION="/root/cernvm-vbox.xml" # Virtual machine XML config file
 CHANGESET="0"
-HOSTNAME="cernvm-redhat5-host"
-GUESTIP="192.168.122.252"
+HOSTNAME="fedora13-vbox-host"
+GUESTIP="192.168.1.139"
 TAPPER_REPORT_SERVER="cernvm-debian6-server"
 REPORTGROUP=selftest-`date +%Y-%m-%d | md5sum | cut -d" " -f1`
 BOOT_ERRORS="boot_error.log"
 NOSEND=0
 NOUPLOAD=0
-<<COMMENT
+
 # A very simple test
 uname -a | grep -q Linux  # example of an ok exit code
 ok $? "The system is running Linux"
@@ -110,16 +109,15 @@ ok $? "Precondition Test 3 - Verify that virtual machine $VMNAME has been starte
 stop_vm $VMNAME
 ok $? "Precondition Test 4 - Verify that virtual machine $VMNAME has been stopped"
 
-# Precondition Test 5 - Verify that the virtual has console support
+# Precondition Test 5 - Verify that the virtual machine has console support
 # Start the virtual machine and verify that it has console support
 start_vm $VMNAME
 has_console_support $VMNAME
-ok $? "Precondition Test 5 - Verify that virtual $VMNAME has console support"
-COMMENT
+ok $? "Precondition Test 5 - Verify that virtual machine $VMNAME has console support"
 
 # Precondition Test 6 - Verify that virtual machine has web interface support
 has_web_interface $GUESTIP web_interface.log
-ok $? "Precondition Test 6 - Verify that virtual machine has web interface support"
+ok $? "Precondition Test 6 - Verify that virtual machine $VMNAME has web interface support"
 add_file web_interface.log
 
 # Precondition Test 7 - Verify that it is possible to login on web interface
@@ -145,24 +143,20 @@ ok $? "CernVM Test Case 3 - Check for correct time / running ntpd"
 
 
 # CernVM Test Case 4 - Create a new user using the CernVM web interface
-# @param $1 - The hostname or ip address for the web interface
-# @param $2 - The name of the new user to create
-# @param $3 - The password for the new user
-# @param $4 - The name of the logfile
 web_create_user $GUESTIP gnuuser VM4l1f3 web_interface_newuser.log
 ok $? "CernVM Test Case 4 - Create a new user using the CernVM web interface"
 add_file web_interface_newuser.log
 
 
-#	CernVM Test Case 5 - Verify that the user is created and can be accessed
-#						 from ssh login
+# CernVM Test Case 5 - Verify that the user is created and can be accessed
+#					   from ssh login
 check_ssh todd $GUESTIP
 ok $? "CernVM Test Case 5 - Verify that the user is created and can be accessed \
 from ssh login"
 
 
-#	CernVM Test Case 6 - Restart through the web interface and check that there
-#						 are no error messages at boot
+# CernVM Test Case 6 - Restart through the web interface and check that there
+#					   are no error messages at boot
 web_restart $GUESTIP web_interface_reboot.log web_restart_boot.log
 ok $? "CernVM Test Case 6 - Restart through the web interface and check that \
 there are no error messages at boot"
